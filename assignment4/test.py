9b22b58063e6c3c3e1e6fc620a8ab004716488a8
vofp <vof@onid.oregonstate.edu>
2015-03-04 14:19:29 -0800

    working on kb

--- assignment4/test.py
+++ assignment4/test.py
@@ -5,7 +5,8 @@ import logic
 
 def main():
 	kb = logic.FluentLogic()
-	print kb.query("formulas(assumptions).  man(george). end_of_list. formulas(goals). mortal(george). end_of_list.")
+	# print kb.query("At(Agent, [0,0], 0).")
+	print kb.query("formulas(sos).man(x) -> mortal(x).man(george).end_of_list.formulas(goals).mortal(george).end_of_list.")
 
 if __name__ == "__main__":
 	main();
